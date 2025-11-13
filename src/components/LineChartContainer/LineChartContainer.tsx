003b7780cf53d1e9e347b6bedea6942ebd184488
alex8023 <alex8023@yandex.ru>
2025-11-13 14:40:52 +0300

    add LineStyle selector & Header

---
+++ src/components/LineChartContainer/LineChartContainer.tsx
@@ -0,0 +1,70 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import type { Data } from '../../lib/prepareData';
+import type { LineStyleMenuItem, PeriodMenuItem, VariantMenuItem } from '../../App';
+import dayjs from 'dayjs';
+
+export function LineChartContainer({
+  data,
+  selectedPeriod,
+  monthTicks,
+  weekTicks,
+  variantMenuItems,
+  selectedVariant,
+  selectedLineStyle,
+}: {
+  data: Data[];
+  selectedPeriod: PeriodMenuItem;
+  monthTicks: string[];
+  weekTicks: string[];
+  variantMenuItems: VariantMenuItem[];
+  selectedVariant: VariantMenuItem;
+  selectedLineStyle: Omit<LineStyleMenuItem, 'area'>;
+}) {
+  return (
+    <LineChart
+      style={{
+        width: '100%',
+        height: '100%',
+        maxHeight: '70vh',
+        aspectRatio: 1.618,
+      }}
+      responsive
+      data={data}
+      margin={{
+        top: 5,
+        right: 0,
+        left: 0,
+        bottom: 5,
+      }}
+    >
+      <CartesianGrid strokeDasharray='3 3' />
+      <YAxis width='auto' tickFormatter={(value) => `${value}%`} />
+      <XAxis
+        dataKey='date'
+        interval='preserveEnd'
+        type='category'
+        ticks={selectedPeriod === 'day' ? monthTicks : weekTicks}
+        tick={{ transform: 'translate(20, 0)' }}
+        tickFormatter={(value) =>
+          selectedPeriod === 'day' ? dayjs(value).format('MMM') : value
+        }
+      />
+      <Tooltip />
+      {variantMenuItems
+        .filter((item) => {
+          if (selectedVariant?.name === 'All variations') {
+            return true;
+          } else return item.name === selectedVariant?.name;
+        })
+        .map((item) => (
+          <Line
+            key={item.id}
+            type={selectedLineStyle as 'linear' | 'monotone'}
+            dot={false}
+            dataKey={item.name}
+            stroke={item.stroke}
+          />
+        ))}
+    </LineChart>
+  );
+}
